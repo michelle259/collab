2e1c9864e2092227b4a61ce92d0cb0b4a1dd6a93
wkdjh <wkdjh1019@gmail.com>
2025-05-12 16:07:28 +0900

    add feature : add employee

--- src/main/java/app/api/controller/EmpApiController.java
+++ src/main/java/app/api/controller/EmpApiController.java
@@ -1,13 +1,29 @@
 package app.api.controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.entity.Emp;
+import app.repository.EmpRepository;
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class EmpApiController {
+	
+	private final EmpRepository empRepository;
 	@GetMapping("/api/emp-test")
 	public String empTest() {
 		return "emp api controller";
+	}
+	
+	
+	@PostMapping("/api/emp")
+	public Emp empInsert(@RequestBody Emp emp) {
+		
+		return empRepository.save(emp);
+		
 	}
 }
