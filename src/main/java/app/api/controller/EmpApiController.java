545b1a5634e7798b9a9f71e83288b5baa466cad5
michelle259 <michelle200323@gmail.com>
2025-05-12 16:25:50 +0900

    update : feature/get_one

--- src/main/java/app/api/controller/EmpApiController.java
+++ src/main/java/app/api/controller/EmpApiController.java
@@ -3,7 +3,11 @@ package app.api.controller;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.entity.Emp;
@@ -23,4 +27,17 @@ public class EmpApiController {
         }
         return employees;
     }
+    
+    @GetMapping("/api/{pid}")
+    public Object getEmployee(@PathVariable Integer pid) {
+        Optional<Emp> employee = empRepository.findById(pid);
+        if (employee.isEmpty()) {
+            return Map.of("msg", "사원정보가 존재하지 않습니다");
+        }
+        return employee;
+        
+       
+    }
+    
+    
 }
